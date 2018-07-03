4205d18528e29eec5ebe0c4121af95fa5e251e84
xpatrickt <patrickrsq@gmail.com>
2018-07-03 10:53:36 -0500

    dasds
    dasdas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Registro RESCA') }}</title>
+    <title>Login RESCA</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
